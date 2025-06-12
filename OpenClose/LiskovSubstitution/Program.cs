eb729f6396ac9946f629ebd9394b81daa897775f
malithwealaarachchi <34570128+malithkavinda@users.noreply.github.com>
2025-06-12 21:11:25 +0530

    add liskovSubsitution

---
+++ OpenClose/LiskovSubstitution/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LiskovSubstitution.WrongSolution;
+
+var contract_emp = new ContractEmployee();
+//This example give calculateBonus methode for contract base employee. This is vialate LSP
+Console.WriteLine(contract_emp.CalculateBonus());
---
+++ OpenClose/LiskovSubstitution/WrongSolution/PermanentEmployee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitution.WrongSolution
+{
+    public class PermanentEmployee : Employee
+    {
+        public override decimal CalculateBonus()
+        {
+            return 30;
+        }
+
+        public override decimal CalculateSalary()
+        {
+            return 500;
+        }
+    }
+}
